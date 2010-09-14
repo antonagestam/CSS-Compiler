03b4bee4b3573e5e5b1d84304fd5df9093077619
Anton Agestam <msn@antonagestam.se>
2010-09-14 22:22:02 +0200

    trying to add some kind of support for singles, this commit IS NOT WORKING

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 		private $rules = array();
 		private $selectors = array();
 		private $compiled_code;
+		private $singles = array();
 		
 		public function __construct($code=false)
 		{
@@ -20,14 +21,19 @@
 		
 		public function init($code)
 		{
+			if(empty($code))
+			{
+				throw new Exception("Must pass string in \$code");
+			}
 			$this->code = $code;
 			$this->compiled = false;
 		}
 		
 		private function find_selectors()
 		{
-			// get a list of selectors
+			// get a list of all the selectors in the css code
 			preg_match_all('#([a-z|\#|\_|\.]+).*?{.*?}#si',$this->code,$matches);
+			// loop the array and save the values in arrays
 			foreach($matches[1] as $index => $selector)
 			{
 				$this->selectors[$selector] = array();
@@ -52,13 +58,62 @@
 					$this->selectors[$index][] = md5($rule);
 				}
 			}
+			
+			$this->clear_code();
+		}
+		
+		/*
+		 * Finds rules that only appear once
+		 */
+		private function is_single($rule_key)
+		{
+			$app = 0;
+			foreach($this->selectors as $selector => $rules)
+			{
+				if(in_array($rule_key,$rules))
+				{
+					$app++;
+				}
+				
+				if($app == 2)
+				{
+					break;
+				}
+			}
+			
+			if($app > 1)
+			{
+				return false;
+			}
+			else
+			{
+				$this->singles[] = $rule_key;
+				return true;
+			}
+		}
+		
+		/*
+		 * Clears the code
+		 */
+		public function clear_code()
+		{
+			$this->code = "";
 		}
 		
 		public function compile()
 		{
+			// parse the css code into something more sofisticated; arrays
 			$this->find_selectors();
+			// inititate an array for rules that only occure once
+			$singles = array();
+			// loop through the rules
 			foreach($this->rules as $index => $rule)
 			{
+				// find all singles
+				if($this->is_single($index))
+				{
+					continue;
+				}
 				foreach($this->selectors as $selector => $rules)
 				{
 					if(in_array($index,$rules))
@@ -67,8 +122,18 @@
 					}
 				}
 				$this->compiled_code = rtrim($this->compiled_code,',');
-				$this->compiled_code .= '{'.$rule.'}';
+				$this->compiled_code .= '{'.$rule.'}'."\n";
 			}
+			
+			// loop through all the rules that only appear once and find out if it's selector has multiple singles and add them to the compiled code
+			foreach($this->singles as $i => $rule_id)
+			{
+				
+			}
+			
+			print_r($this->singles);
+			print_r($this->selectors);
+			print_r($this->rules);
 			$this->compiled = true;
 			
 			return $this;
